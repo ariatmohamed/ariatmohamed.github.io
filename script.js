6f754c9d4fcef5c34919f827073e09d0b91f9ee4
ariatmohamed <mohamedhajamer7@gmail.com>
2024-10-27 21:49:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-index.html 
-index.HTML
-index.javaScript
+document.getElementById('processButton').addEventListener('click', function() {
+    const videoInput = document.getElementById('videoUpload');
+    const videoFile = videoInput.files[0];
+
+    if (videoFile) {
+        // هنا يمكنك إضافة كود لتحويل الفيديو إلى نص وترجمته.
+        alert("تم رفع الفيديو: " + videoFile.name);
+        // أضف الكود الخاص بالتحويل والترجمة هنا.
+    } else {
+        alert("يرجى رفع ملف فيديو.");
+    }
+});
